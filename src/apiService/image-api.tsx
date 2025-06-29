afa4558d27587e700284a155f16f6c44c51c6fe4
Taras Pererva <taraspererva@gmail.com>
2025-06-29 18:38:24 +0300

    fix1

--- src/apiService/image-api.tsx
+++ src/apiService/image-api.tsx
@@ -1,20 +1,20 @@
-import axios, { AxiosResponse } from "axios";
-import { ImageResponse } from "./image-api.types";
+import axios, { AxiosResponse } from 'axios';
+import { ImageResponse } from './image-api.types';
 
-const API_KEY = "9w97Pb99jXHhNYoe8GBo9dM00UNydqvm8EukWNyUwo0";
+const API_KEY = 'mskXwYk7bCouuxf64aJE3hOqbVNI95fUDMc0-66QkWM';
 
-axios.defaults.baseURL = "https://api.unsplash.com/search/photos";
+axios.defaults.baseURL = 'https://api.unsplash.com/search/photos';
 
 const fetchImages = async (
   currentPage: number,
   searchQuery: string
 ): Promise<ImageResponse> => {
-  const response: AxiosResponse<ImageResponse> = await axios.get("", {
+  const response: AxiosResponse<ImageResponse> = await axios.get('', {
     params: {
       query: searchQuery,
       page: currentPage,
       per_page: 15,
-      orientation: "landscape",
+      orientation: 'landscape',
       client_id: API_KEY,
     },
   });
